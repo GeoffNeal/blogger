d95289b90985f23a561268f64165debaaaa54689
Geoffrey Neal <geoffrey_neal@hotmail.com>
2016-10-04 16:31:13 +0100

    Added angular-material stylesheet in the css folder so that it can be accessed by the express static server, which is currently configured to serve from the 'public' directory.

--- app/app.js
+++ app/app.js
@@ -4,5 +4,6 @@ var angular = require("angular");
 
 angular.module("blogger", ["ngAnimate", "ngMaterial"]);
 
+// require("./../node_modules/angular-material/angular-material.min.js");
 require("./controllers/mainCtrl.js");
 require("./services/dataService.js");
